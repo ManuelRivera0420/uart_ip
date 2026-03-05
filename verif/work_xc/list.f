d1f6422773517330fcd981e1dc21d3580d8b50bc
ManuelRivera0420 <manuel2004rj@gmail.com>
2026-03-05 15:16:18 -0600

    updated list.f for xcelium runs

--- verif/work_xc/list.f
+++ verif/work_xc/list.f
@@ -1,16 +1,24 @@
     ../property_defines.svh
     ../../rtl/defines.svh
-# ============================ rtl
+# ============================ SRAM
+    ../../rtl/rtl_mixedsram/sram_cell.sv
+    ../../rtl/rtl_mixedsram/write_driver.sv
+    ../../rtl/rtl_mixedsram/decoder.sv
+    ../../rtl/rtl_mixedsram/sense_amp.sv
+    ../../rtl/rtl_mixedsram/cell_array.sv
+    ../../rtl/rtl_mixedsram/sipo.sv
+    ../../rtl/rtl_mixedsram/sram_ip.sv
+    ../../rtl/rtl_mixedsram/wb/wb_sram.sv
+
+  # ============================ rtl UART
     ../../rtl/uart_clk_gen.sv
     ../../rtl/uart_edge_detector.sv
-    ../../rtl/uart_status_reg.sv
     ../../rtl/uart_control_reg.sv
     ../../rtl/uart_tnsm.sv
     ../../rtl/uart_recv.sv
-    ../../rtl/uart_ip.sv
-    ../../fv/fv_uart_recv.sv
-    ../../fv/fv_uart_tnsm.sv
-    ../../fv/fv_uart_clk_gen.sv
+    ../../rtl/fsm_instruction_loader.sv
+    ../../rtl/wb_master.sv
+    ../../rtl/uart_ip.sv 
 # ============================ verification
     ../uart_ip_interface.sv
     ../uart_ip_tb.sv
